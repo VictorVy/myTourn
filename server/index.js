5854b3c3f4763ece4066e5c071a5f7b85546ecab
Victor Yao <victoryao1039@gmail.com>
2024-03-14 21:34:35 -0700

    set up oracle-db and verify connection works (protected host; must forward/tunnel)

--- server/index.js
+++ server/index.js
@@ -1,4 +1,61 @@
 import express from "express";
+import oracledb from "oracledb";
+
+oracledb.outFormat = oracledb.OUT_FORMAT_OBJECT;
+
+// with help from guide at https://github.students.cs.ubc.ca/CPSC304/CPSC304_Node_Project/
+
+const dbConfig = {
+    user: process.env.ORA_USER,
+    password: process.env.ORA_PASSWORD,
+    connectString: process.env.ORA_HOST + ":" + 
+                    process.env.ORA_PORT + "/" + 
+                    process.env.ORA_DBNAME,
+    poolMin: 1,
+    poolMax: 3,
+    poolIncrement: 1,
+    poolTimeout: 60
+};
+
+async function initializePool() {
+    try {
+        await oracledb.createPool(dbConfig);
+        console.log("Connection pool initialized");
+    } catch (err) {
+        console.error("init error: " + err.message);
+    }
+}
+
+async function closePool() {
+    console.log("Closing");
+    try {
+        await oracledb.getPool().close(10);
+        console.log("Pool closed");
+        process.exit(0);
+    } catch (err) {
+        console.error(err.message);
+        process.exit(1);
+    }
+}
+
+process
+    .once("SIGTERM", closePool)
+    .once("SIGINT", closePool);
+
+initializePool().then(async () => {
+    let connection = await oracledb.getConnection();
+    console.log("Connection started");
+
+    const names = await connection.execute(
+        `SELECT owner, table_name FROM all_tables`
+    );
+
+    console.log(names);
+    await connection.close();
+    console.log("Connection closed");
+});
+
+
 const app = express();
 
 app.get("/api", (req, res) => {
