8cacd41a8c758daed266bb3320d2b87d363e4dd1
Victor Yao <victoryao1039@gmail.com>
2024-04-03 19:41:01 -0700

    expose generic operation endpoints: query, insert, update, delete

--- server/index.js
+++ server/index.js
@@ -3,6 +3,11 @@ import router from "./router.js";
 
 const app = express();
 app.use(express.static("dist"));
+app.use((req, res) => {
+    console.log(req.method, req.url);
+    req.next();
+});
+app.use(express.json());
 app.use(router);
 
 app.listen(process.env.PORT, () => console.log("Server is listening on port " + process.env.PORT + "..."));
